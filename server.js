d64b0b685eba60a727cbf40b274e4d0f32577720
pallavi-mane_globant <pallavi.mane@globant.com>
2025-05-27 15:37:43 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+
+// app.get('/',(req,res)=>{
+//     res.
+// })
+
+app.listen(3000,()=>{
+    console.log("running node app")
+})
